8e5d9600581c3edab571bbfa01b6d62e12308599
Thafseena <thafseenamkd121996@gmail.com>
2023-06-30 23:37:19 +0530

    value notifier started code

--- lib/db/functions/db_functions.dart
+++ lib/db/functions/db_functions.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:hiveapp/db/model/data_model.dart';
 
 // List<StudentModel> studentList=[];
-ValueNotifier<List<StudentModel>> studentList=ValueNotifier([]);
+ValueNotifier<List<StudentModel>> studentListNotifier=ValueNotifier([]);
 
 
 void addStudent(StudentModel value){
   // studentList.add(value);
-  studentList.add(value);
+  studentListNotifier.value.add(value);
 }
